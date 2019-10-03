f6630dafadbc4e9256d898ea709422d2c85b9e77
ekaterina-ph <khudiakova.ev@phystech.edu>
2019-10-04 02:33:19 +0300

    fixed

---
+++ dishwash.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int tableSize = atoi(argv[1]);
+
+	return 0;
+}
